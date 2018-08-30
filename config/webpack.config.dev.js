40c5b7933f9e2246b478a9aeb5763275ca50a0e1
Will Lin <will_lin@jhancock.com>
2018-08-29 20:10:03 -0400

    added sass

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -170,7 +170,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.(s*)css$/,
             use: [
               require.resolve('style-loader'),
               {
@@ -199,6 +199,7 @@ module.exports = {
                   ],
                 },
               },
+              require.resolve('sass-loader'),
             ],
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
